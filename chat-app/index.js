55c2c27cc421f85a152b0895e226af4635c9d282
Nishan9t <pandeynisha9t@gmail.com>
2023-10-05 10:01:57 +0530

    number of total clients is now working

--- chat-app/index.js
+++ chat-app/index.js
@@ -10,15 +10,45 @@ const app = express();
 //so you can not directly do app.listen
 
 const server = http.createServer(app);
+
+let socketsConnected = new Set()
+
 //make instance of io like input output
 
 const io= new Server(server);
+
+io.on("connection",onConnected)
+
+function onConnected(socket){
+    //console.log(socket.id)
+    socketsConnected.add(socket.id);
+
+
+    io.emit('clients-total',socketsConnected.size)
+
+
+    socket.on('disconnect',()=>{
+        console.log(socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total',socketsConnected.size)
+    })
+}
+
+
+
+
+
+
+
+
 //here server is server created with help of http
 
 
 //Socket.io
+//connection means when socket is connected
+// io.on('connection', (socket) => {
 
-io.on('connection', (socket) => {
+
     //here socket is basically a user or client 
     // console.log('a new user has connected',socket.id);
 
@@ -28,13 +58,13 @@ io.on('connection', (socket) => {
     //   });
 
 //server is getting message
-    io.on('connection', (socket) => {
-        socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-        });
-    });
+    // io.on('connection', (socket) => {
+    //     socket.on('chat message', (msg) => {
+    //     io.emit('chat message', msg);
+    //     });
+    // });
 
-  });
+//   });
 
 
 
